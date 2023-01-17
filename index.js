3c7a0d373875ce7f61d1c138d439093895c4fef2
lgxxhi <luisgsilva95@gmail.com>
2023-01-17 18:12:32 -0500

    Javascript on Machine Lab

--- index.js
+++ index.js
@@ -4,7 +4,35 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+
+    //console.log(process.argv)
+
+    if (process.argv.length < 3) {
+        return "No operation provided..."
+    } else if (process.argv.length === 3) {
+        return "No numbers provided..."
+    } else if (process.argv[2] !== "plus" && process.argv[2] !== "minus") {
+        return `Invalid operation: ${process.argv[2]}`
+    }
+
+    let sum = 0
+    if (process.argv[2] === "plus") {
+        for (let i = 3; i < process.argv.length; i++) {
+            sum += Number(process.argv[i])
+        }
+    } else if (process.argv[2] === "minus") {
+        let sum = Number(process.argv[3])
+        for (let i = 4; i < process.argv.length; i++) {
+            sum -= (Number(process.argv[i]))
+            //console.log(sum)
+        }
+        return sum
+    }
+    return sum
+
+}
+
 
 // Don't change anything below this line.
 module.exports = calculator;
