c28d330b547599b678ff0762cde648c836c94c2f
Jibon P <jibonp@jibons-air.mynetworksettings.com>
2023-01-17 14:06:35 -0500

    New updated file

--- index.js
+++ index.js
@@ -5,51 +5,33 @@
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
 function calculator() {
-    if (process.argv.length < 3) {
-        return "No operation provided...";
-        
-    }
-    if (process.argv.length < 4) {
-        return "No numbers provided...";
-        
-    }
-    
-    
-    const operation = process.argv[2];
-    if (operation !== "plus" && operation !== "minus"){
-        return `Invalid operation: ${operation}`;
-
-    }
-    
-    const numbers = process.argv.slice(3).map(Number);
-    let result;
-
-    switch (operation) {
-        case "plus":
-            result = numbers.reduce((a, b) => a + b);
-            break;
-        case "minus":
-            result = numbers.reduce((a, b) => a - b);
-            break;
-        case "multiply":
-            result = numbers.reduce((a, b) => a * b);
-            break;
-        case "divide":
-            if(numbers.length>1){
-                result = numbers.reduce((a, b) => a / b);
-            }else{
-                console.log("Invalid operation: divide");
-                return;
-            }
-            break;
-        default:
-            console.log("Invalid operation: ", operation);
-            return;
-    }
-    return result;
+let num = 0;
+if (process.argv.length < 3) {
+    return "No operation provided...";
+}
+if (process.argv.length < 4) {
+    return "No numbers provided...";
 }
 
-calculator();
+const operation = process.argv[2];
+if (operation !== "plus" && operation !== "minus"){
+    return `Invalid operation: ${operation}`;
+}
+
+if(operation === 'plus'){
+    for(let i = 3; i < process.argv.length; i++){
+        num += Number(process.argv[i]);
+    }
+}
+if(operation === 'minus'){
+    num = Number(process.argv[3]);
+    for(let i = 4; i < process.argv.length; i++){
+        num -= Number(process.argv[i]);
+    }
+}
+return num;
+}
+
 
 
 // Don't change anything below this line.
